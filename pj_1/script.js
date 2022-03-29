49fd5d6c14126ab0e91c9c4ba9a3497626a5ac4d
alejolagosm <falame94@hotmail.com>
2022-03-29 09:17:54 -0500

    Finished first mini project: Form validator

--- pj_1/script.js
+++ pj_1/script.js
@@ -16,40 +16,57 @@ function showSuccess(input, message) {
   formControl.className = 'form-control success';
 }
 
-function isValidEmail(email) {
-  return String(email)
+const getFieldName = input => {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+};
+
+const checkRequired = inputArray => {
+  inputArray.forEach(input => {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+};
+
+function checkEmail(input) {
+  const check = String(input.value.trim())
     .toLowerCase()
     .match(
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
+
+  if (check) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 }
 
+const checkLength = (input, min, max) => {
+  if (input.value.length < min || input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be between ${min} and ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+};
+
+const checkMatchPasswords = (input1, input2) => {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
+};
 // Event listeners
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-  if (username.value === '') {
-    showError(username, 'Username is required');
-  } else {
-    showSuccess(username);
-  }
 
-  if (email.value === '') {
-    showError(email, 'Email is required');
-  } else if (!isValidEmail(email.value)) {
-    showError(email, 'Email is not valid');
-  } else {
-    showSuccess(email);
-  }
-
-  if (password.value === '') {
-    showError(password, 'Password is required');
-  } else {
-    showSuccess(password);
-  }
-
-  if (password.value === '') {
-    showError(password, 'password is required');
-  } else {
-    showSuccess(password);
-  }
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkEmail(email);
+  checkLength(password, 8, 12);
+  checkMatchPasswords(password, password2);
 });
